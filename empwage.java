032f54e3eca676dfe3938eddfc2b398f1eaa5921
sagarwaghamode <dipakwaghamode@gmail.com>
2020-06-23 23:30:41 +0530

    Added empwage

--- empwage.java
+++ empwage.java
@@ -1,16 +1,21 @@
 public class empwage
 {
+
+//		System.out.println("Welcome to EmpWageComputation");
+
+	int wages=0;
+	int fulltime=8;
+	int wage_per_hr=20;
+public int calculatewage(int fulltime, int wage_per_hr){
+	int wage = wage_per_hr * fulltime;
+	return wage;
+}
+
 	public static void main(String args[])
 	{
+	empwage Employeewage = new  empwage();
+	System.out.println("Employee wage : " + Employeewage.calculatewage(Employeewage.fulltime , Employeewage.wage_per_hr));
 
-		System.out.println("Welcome to EmpWageComputation");
-	int isPresent=1;
-	int isAbsent=0;
-	double AttendanceCheck = Math.floor( Math.random() * 10) % 2;
-	if ( AttendanceCheck == isPresent )
-	System.out.println("Employee is present");
-else
-	System.out.println("Employee is Absent");
 
 
 	}
