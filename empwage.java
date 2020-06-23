46f6052e8618282b59ca2eacba212bde56635468
sagarwaghamode <dipakwaghamode@gmail.com>
2020-06-23 21:04:40 +0530

    Added Attendance check

--- empwage.java
+++ empwage.java
@@ -4,6 +4,14 @@ public class empwage
 	{
 
 		System.out.println("Welcome to EmpWageComputation");
+	int isPresent=1;
+	int isAbsent=0;
+	double AttendanceCheck = Math.floor( Math.random() * 10) % 2;
+	if ( AttendanceCheck == isPresent )
+	System.out.println("Employee is present");
+else
+	System.out.println("Employee is Absent");
+
 
 	}
 
