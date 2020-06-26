89f585b86c21d373fb1392f35ab7dbc4857960ed
sagarwaghamode <dipakwaghamode@gmail.com>
2020-06-26 17:51:38 +0530

    Added wages for month

--- empwage.java
+++ empwage.java
@@ -11,12 +11,12 @@ public class empwage
 	int fullDayHrs=8;
 	int isParttime=0;
 	int isFulltime=1;
-	int totalSalary=0;
-	int empRate=10;
-	int empHrs=0;
+	int totalSalary=0,totalDays=20;
+	int empRate=10,empHrs=0;
 
-	double empCheck = Math.floor( Math.random() * 10) % 2;
-	
+
+	int empCheck = (int)Math.floor( Math.random() * 10) % 2;
+
 	if ( empCheck == isPresent )
 	{
 	System.out.println("Employee is present");
@@ -24,26 +24,35 @@ public class empwage
 	System.out.println("Daily Employee wage salary is: " +salary);
 	}
 
-	 int choice = (int)empCheck;
-
-		switch(choice)
+	 for(int day=1; day<=totalDays;day++)
 		{
-			case 1:
-				empHrs = 16;
-				System.out.println("Employee Working full time.");
-				break;
-			case 2:
-				empHrs = 8;
-				System.out.println("Employee Working part time.");
-				break;
-			default:
-				System.out.println("Employee is absent");
-				break;
+			switch(empCheck)
+			{
+				case 1:
+					empHrs = 16;
+					break;
+				case 2:
+					empHrs = 8;
+					break;
+				default:
+					empHrs = 0;
+					break;
+			}
+
+		 	salary = empHrs * empRate;
+			totalSalary = totalSalary + salary + salary;
 		}
 
-		totalSalary = empHrs * empRate;
+		if(empCheck == 0)
+		{
+			System.out.println("Employee Working part time");
+			System.out.println("Total Salary = "+totalSalary);
+		}
+		else
+		{
+			System.out.println("Employee Working full time");
 		System.out.println("Total Salary = "+totalSalary);
+		}
 	}
-	
 
 }
