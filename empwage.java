6c4bd577f809ab7d83a73be8caf7cc40ff51f066
sagarwaghamode <dipakwaghamode@gmail.com>
2020-06-26 22:52:03 +0530

    Check Emp Present or not

--- empwage.java
+++ empwage.java
@@ -1,19 +1,20 @@
 public class empwage
 {
-	public static void main(String args[])
+	public static void main(String args[])	
 	{
-
 		System.out.println("Welcome to EmpWageComputation");
-	int isPresent=1;
-	int isAbsent=0;
-	double AttendanceCheck = Math.floor( Math.random() * 10) % 2;
-	if ( AttendanceCheck == isPresent )
-	System.out.println("Employee is present");
-else
-	System.out.println("Employee is Absent");
 
 
+		int isPresent = 1;
+		int isAbsent = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isPresent)
+		{	
+			 System.out.println("Employee is Present");
+		}	
+		else
+		{
+			System.out.println("Employee is absent");
+		}
 	}
-
-
 }
