846f5584d49c6e1b843eb4e6a94f21fc4c8bc800
sagarwaghamode <dipakwaghamode@gmail.com>
2020-06-26 22:43:14 +0530

    Added Part time wage

--- empwage.java
+++ empwage.java
@@ -1,46 +1,20 @@
 public class empwage
 {
-	public static void main(String args[])
+	public static void main(String args[])	
 	{
-
-		System.out.println("Welcome to EmpWageComputation");
-	int isPresent=1;
-	int isAbsent=0;
-	int salary=0;
-	int wagePerHr=20;
-	int fullDayHrs=8;
-	int isParttime=0;
-	int isFulltime=1;
-	int totalSalary=0;
-	int empRate=10;
-	int empHrs=0;
-
-	double AttendanceCheck = Math.floor( Math.random() * 10) % 2;
-	
-	if ( AttendanceCheck == isPresent )
-	{
-	System.out.println("Employee is present");
-	salary = wagePerHr * fullDayHrs;
-	System.out.println("Daily Employee wage salary is: " +salary);
-	}
-
-	if(AttendanceCheck == isFulltime)
-		{
-			empHrs = 16;
-			System.out.println("Employee Working full time");
-		}
-		else if(AttendanceCheck == isParttime)
-		{
-			empHrs = 8;
-			System.out.println("Employee is Present and its working part time ");
-		}
+		int partTime=1;
+		int fullTime=2;
+		int empRatePerHr=20;
+		int empHrs=0;
+		int empWage=0;
+		double empCheck=Math.floor(Math.random() * 10) % 3;
+		if (empCheck == partTime)
+			empHrs=4;
+		else if (empCheck == fullTime)
+			empHrs=8;
 		else
-		{
-			System.out.println("Employee is absent");
-		}
-		totalSalary = empHrs * empRate;
-		System.out.println("Total Salary = "+totalSalary);
+			empHrs=0;
+		empWage=empHrs * empRatePerHr;
+		System.out.println("Employee wage is :"+empWage);
 	}
-	
-
 }
